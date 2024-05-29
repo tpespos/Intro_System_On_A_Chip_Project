a0186d678342d27a626f61970bc6a42f7c775277
Tommy Esposito <35857516+tpespos@users.noreply.github.com>
2024-05-29 00:57:40 -0700

    Update ACC.v
    removed the reset command because it's not part of the module

--- Accumulator/ACC.v
+++ Accumulator/ACC.v
@@ -1,16 +1,13 @@
-module acc (
+module ACC (
     input wire clk,
-    input wire reset,
+    input wire clb,
     input wire load_acc,
     input wire [7:0] data_in,
     output reg [7:0] acc_out
 );
 
-    always @(posedge clk or posedge reset) begin
-        if (reset) begin
-            acc_out <= 8'b0;
-        end 
-    else if (load_acc) begin
+    always @(posedge clk) begin
+	if (load_acc) begin
             acc_out <= data_in;
         end
     end
