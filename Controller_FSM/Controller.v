3a0b42f768672938fdf27b82701834f320ac3bbb
rlanders7 <landersryan27@gmail.com>
2024-05-29 21:11:44 -0700

    Update Controller.v

--- Controller_FSM/Controller.v
+++ Controller_FSM/Controller.v
@@ -26,65 +26,192 @@ LoadPC <= 0;
 LoadReg <= 0;
 LoadAcc <= 0;
 SelAcc <= 2'b00;
-SelALU <= 4'b0000;
+SelALU <= Opcode;
 //end
 
 
 //////////////////////////////////////////////////// ALU Operations ////////////////////////////////////////////////////
 if (Opcode == 4'b0001) begin //Add
-//do things
+	LoadIR <= 1;
+	IncPC <= 1;
+	SelPC <= 0;
+	LoadPC <= 0;
+	LoadReg <= 0;
+	LoadAcc <= 1;
+	SelAcc <= 2'b01;
 
 end else if (Opcode == 4'b0010) begin //Sub
-//do things
+	LoadIR <= 1;
+	IncPC <= 1;
+	SelPC <= 0;
+	LoadPC <= 0;
+	LoadReg <= 0;
+	LoadAcc <= 1;
+	SelAcc <= 2'b01;
 
 end else if (Opcode == 4'b0011) begin //Nor
-//do things
+	LoadIR <= 1;
+	IncPC <= 1;
+	SelPC <= 0;
+	LoadPC <= 0;
+	LoadReg <= 0;
+	LoadAcc <= 1;
+	SelAcc <= 2'b01;
 
 end else if (Opcode == 4'b1100) begin //Shift right
-//do things
+	LoadIR <= 1;
+	IncPC <= 1;
+	SelPC <= 0;
+	LoadPC <= 0;
+	LoadReg <= 0;
+	LoadAcc <= 1;
+	SelAcc <= 2'b01;
 
 end else if (Opcode == 4'b1011) begin //Shift left
-//do things
+	LoadIR <= 1;
+	IncPC <= 1;
+	SelPC <= 0;
+	LoadPC <= 0;
+	LoadReg <= 0;
+	LoadAcc <= 1;
+	SelAcc <= 2'b01;
 
 end 
 //////////////////////////////////////////////////// Register/ACC Load ////////////////////////////////////////////////////
-else if (Opcode == 4'b0100) begin //reg -> acc load
-//do things
+else if (Opcode == 4'b0100) begin //reg -> acc
+	LoadIR <= 1;
+	IncPC <= 1;
+	SelPC <= 0;
+	LoadPC <= 0;
+	LoadReg <= 0;
+	LoadAcc <= 1;
+	SelAcc <= 2'b10;
 
 end else if (Opcode == 4'b0101) begin //ACC -> reg
-//do things
+	LoadIR <= 1;
+	IncPC <= 1;
+	SelPC <= 0;
+	LoadPC <= 0;
+	LoadReg <= 1;
+	LoadAcc <= 0;
+	SelAcc <= 2'b00;
 
 end else if (Opcode == 4'b1101) begin //Imidiate (IMM) -> ACC
-//do things
+	LoadIR <= 1;
+	IncPC <= 1;
+	SelPC <= 0;
+	LoadPC <= 0;
+	LoadReg <= 0;
+	LoadAcc <= 1;
+	SelAcc <= 2'b00;
 
 end 
 //////////////////////////////////////////////////// Branch Instructions ////////////////////////////////////////////////////
 else if (Opcode == 4'b0110) begin //if ACC = 0, reg -> PC (jump if 0)...else PC+1 -> PC
-//do things
+	if (Z == 0) begin //branch = yes
+		LoadIR <= 1;
+		IncPC <= 0;
+		SelPC <= 0;
+		LoadPC <= 1;
+		LoadReg <= 0;
+		LoadAcc <= 0;
+		SelAcc <= 2'b00;
+
+	end else begin //branch = no
+		LoadIR <= 1;
+		IncPC <= 1;
+		SelPC <= 0;
+		LoadPC <= 0;
+		LoadReg <= 0;
+		LoadAcc <= 0;
+		SelAcc <= 2'b00;
+	end
 
 end else if (Opcode == 4'b0111) begin //if ACC = 0, Imm -> PC (jump if 0) else PC+1 -> PC
-//do things
+	if (Z == 0) begin //branch = yes
+		LoadIR <= 1;
+		IncPC <= 0;
+		SelPC <= 1;
+		LoadPC <= 1;
+		LoadReg <= 0;
+		LoadAcc <= 0;
+		SelAcc <= 2'b00;
 
+	end else begin //branch = no
+		LoadIR <= 1;
+		IncPC <= 1;
+		SelPC <= 0;
+		LoadPC <= 0;
+		LoadReg <= 0;
+		LoadAcc <= 0;
+		SelAcc <= 2'b00;
+	end
+	
 end else if (Opcode == 4'b1000) begin //if ACC <0, Reg -> PC (jump if carry) else PC +1 -> PC
-//do things
+	if (C == 1) begin //branch = yes
+		LoadIR <= 1;
+		IncPC <= 0;
+		SelPC <= 0;
+		LoadPC <= 1;
+		LoadReg <= 0;
+		LoadAcc <= 0;
+		SelAcc <= 2'b00;
 
+	end else begin //branch = no
+		LoadIR <= 1;
+		IncPC <= 1;
+		SelPC <= 0;
+		LoadPC <= 0;
+		LoadReg <= 0;
+		LoadAcc <= 0;
+		SelAcc <= 2'b00;
+	end
+	
 end else if (Opcode == 4'b1010) begin //if ACC <0, Imm -> PC (jump if carry) else PC +1 -> PC
-//do things
+	if (C == 1) begin //branch = yes
+		LoadIR <= 1;
+		IncPC <= 0;
+		SelPC <= 1;
+		LoadPC <= 1;
+		LoadReg <= 0;
+		LoadAcc <= 0;
+		SelAcc <= 2'b00;
 
+	end else begin //branch = no
+		LoadIR <= 1;
+		IncPC <= 1;
+		SelPC <= 0;
+		LoadPC <= 0;
+		LoadReg <= 0;
+		LoadAcc <= 0;
+		SelAcc <= 2'b00;
+	end
+	
 end
 //////////////////////////////////////////////////// Control Operations ////////////////////////////////////////////////////
 else if (Opcode == 4'b0000) begin //NOP
-//do things
+	LoadIR <= 1;
+	IncPC <= 1;
+	SelPC <= 0;
+	LoadPC <= 0;
+	LoadReg <= 0;
+	LoadAcc <= 0;
+	SelAcc <= 2'b00;
 
 end else if (Opcode == 4'b1111) begin //HALT
-//do things
+	LoadIR <= 0;
+	IncPC <= 0;
+	SelPC <= 0;
+	LoadPC <= 0;
+	LoadReg <= 0;
+	LoadAcc <= 0;
+	SelAcc <= 2'b00;
 
 end //end ifs
 
 end//end always
 
 endmodule
-
 
 
 
